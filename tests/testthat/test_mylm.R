cc5ce361ae7e2cd29d0f1270e460f59440a37bb0
Ziman Jiang <zmjiang@umich.edu>
2021-12-03 17:14:48 -0500

    add test cases

--- tests/testthat/test_mylm.R
+++ tests/testthat/test_mylm.R
@@ -1,30 +1,95 @@
-#test function
+##test function
 test_mylm <- function(mymodel, lmmodel){
-  diff <- 1e-6
+  diff <- 1e-5
   #coefficients
   expect_equal( mymodel$coefficients[,1],
                 lmmodel$coefficients[,1],
                 tolerance=diff )
   #residuals
-  expect_equal( mymodel$fitted_value,
-                lmmodel$residuals,
+    #Since the residual is equal to the outcome minus the fitted value, it guarantees the equality of fitted values
+  expect_equal( sum(mymodel$Residuals-lmmodel$residuals),0,
+                tolerance=diff )
+  #standard error
+  expect_equal( mymodel$coefficients[,2],
+                lmmodel$coefficients[,2],
+                tolerance=diff )
+  #standard error
+  expect_equal( mymodel$coefficients[,2],
+                lmmodel$coefficients[,2],
+                tolerance=diff )
+  #t statistic
+  expect_equal( mymodel$coefficients[,3],
+                lmmodel$coefficients[,3],
+                tolerance=diff )
+  #p-value of t test
+  expect_equal( mymodel$coefficients[,4],
+                lmmodel$coefficients[,4],
+                tolerance=diff )
+  #residual standard error
+  expect_equal( mymodel$RSE,
+                lmmodel$sigma,
+                tolerance=diff )
+  #R squared
+  expect_equal( mymodel$R_squared,
+                lmmodel$r.squared,
+                tolerance=diff )
+  #adjusted R squared
+  expect_equal( mymodel$adjusted_R_squared,
+                lmmodel$adj.r.squared,
+                tolerance=diff )
+  #F statistic
+  expect_equal( mymodel$f_value,
+                unname(lmmodel$fstatistic[1]),
+                tolerance=diff )
+  #degree of freedom
+  expect_equal( mymodel$df[1],
+                lmmodel$df[1],
+                tolerance=diff )
+  expect_equal( mymodel$df[2],
+                lmmodel$df[2],
                 tolerance=diff )
 }
+
+##tests
 #SLR
 test_that("mylm works for the simple linear regression",{
   mylm1 <- mylm(mpg~wt,mtcars,"nothing")
   lm1 <- summary(lm(mpg~wt,mtcars))
   test_mylm (mylm1, lm1)
 })
+
 #MLR
-mylm2 <- mylm(mpg~wt+drat,mtcars,"nothing")
-lm2 <- summary(lm(mpg~wt+drat,mtcars))
+test_that("mylm works for the multiple linear regression",{
+  mylm2 <- mylm(mpg~wt+drat,mtcars,"nothing")
+  lm2 <- summary(lm(mpg~wt+drat,mtcars))
+  test_mylm (mylm2, lm2)
+})
+
 #MLR with categorical covariate
-mylm3 <- mylm(mpg~wt+drat+factor(gear),mtcars,"nothing")
-lm3 <- summary(lm(mpg~wt+drat+factor(gear),mtcars))
+test_that("mylm works for the multiple linear regression with categorical covariate",{
+  mylm3 <- mylm(mpg~wt+drat+factor(gear),mtcars,"nothing")
+  lm3 <- summary(lm(mpg~wt+drat+factor(gear),mtcars))
+  test_mylm (mylm3, lm3)
+})
+
 #MLR with interaction term
-mylm4 <- mylm(mpg~wt+drat+I(wt*drat),mtcars,"nothing")
-lm4 <- summary(lm(mpg~wt+drat+I(wt*drat),mtcars))
+test_that("mylm works for the multiple linear regression with interaction term",{
+  mylm4 <- mylm(mpg~wt+drat+I(wt*drat),mtcars,"nothing")
+  lm4 <- summary(lm(mpg~wt+drat+I(wt*drat),mtcars))
+  test_mylm (mylm4, lm4)
+})
+
 #MLR with transformed covariate
-mylm5 <- mylm(mpg~wt+drat+I(wt^2),mtcars,"nothing")
-lm5 <- summary(lm(mpg~wt+drat+I(wt^2),mtcars))
+test_that("mylm works for the multiple linear regression with transformed covariate",{
+  mylm5 <- mylm(mpg~wt+drat+I(wt^2),mtcars,"nothing")
+  lm5 <- summary(lm(mpg~wt+drat+I(wt^2),mtcars))
+  test_mylm (mylm5, lm5)
+})
+
+#MLR with transformed covariate
+# test_that("mylm works for the multiple linear regression without intercept",{
+#   mylm6 <- mylm(mpg~-1+wt+drat,mtcars,"nothing")
+#   lm6 <- summary(lm(mpg~-1+wt+drat,mtcars))
+#   test_mylm (mylm6, lm6)
+# })
+
